0e41f7f1ba25511e6f0100e4e96be7dee664e36d
s3601172 <30820975+s3601172@users.noreply.github.com>
2018-04-05 17:20:14 +1000

    started Staff class

--- HRSystem/src/hr/staff/Position.java
+++ HRSystem/src/hr/staff/Position.java
@@ -1,28 +1,29 @@
 package hr.staff;
 
 public class Position {
+
 	double pWage;
 	boolean pFilledStatus;
 	String pType;
-	
+
 	public Position(double wage, boolean filledStatus, String type) {
 		pWage = wage;
 		pFilledStatus = filledStatus;
 		pType = type;
 	}
-	
+
 	public double getWage() {
 		return this.pWage;
 	}
-	
+
 	public void setWage(double wage) {
 		wage = this.pWage;
 	}
-	
+
 	public boolean isFilled() {
 		return this.pFilledStatus;
 	}
-	
+
 	public void setFilled(boolean filledStatus) {
 		filledStatus = this.pFilledStatus;
 	}
@@ -30,7 +31,7 @@ public class Position {
 	public String getType() {
 		return this.pType;
 	}
-	
+
 	public void setType(String type) {
 		type = pType;
 	}
--- HRSystem/src/hr/staff/Staff.java
+++ HRSystem/src/hr/staff/Staff.java
@@ -2,4 +2,48 @@ package hr.staff;
 
 public class Staff {
 
+	int sID;
+	int sPhone;
+	String sEmail;
+	// split into username and password?
+	String sLoginDetails;
+	boolean[] sAvailability;
+
+	public Staff(int ID, int phone, String email, String loginDetails, boolean[] availability) {
+
+	}
+
+	public int getID() {
+		return this.sID;
+	}
+
+	public void setID(int ID) {
+		ID = this.sID;
+	}
+
+	public int getPhone() {
+		return this.sPhone;
+	}
+
+	public void setPhone(int phone) {
+		phone = this.sPhone;
+	}
+
+	public String getEmail() {
+		return this.sEmail;
+	}
+
+	public void setEmail(String email) {
+		email = this.sEmail;
+	}
+
+	public String getLoginDetails() {
+		return this.sLoginDetails;
+	}
+
+	public void setLoginDetails(String loginDetails) {
+		loginDetails = this.sLoginDetails;
+	}
+
+	//not sure how to do getters and setters for the availability
 }
